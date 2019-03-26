4da997f3855b3c97271cbf14133a70f873264243
Alexander Kozhaev <alekzoid@gmail.com>
2019-03-26 22:02:27 +1000

    add server models

--- server/models.py
+++ server/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Server(models.Model):
+    ip = models.CharField(max_length=15)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name + ' (%s)' % self.ip
+
+
+class Log(models.Model):
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    is_online = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '  %s ' % self.date + self.server.__str__() + ' ' + ('up' if self.is_online else 'down')
